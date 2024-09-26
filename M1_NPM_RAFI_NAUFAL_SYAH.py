968d8888384303124ea1b2c62fa34657c91954fc
Rafi Naufal Syah <148581746+rafinaufal04@users.noreply.github.com>
2024-09-27 01:57:57 +0700

    Add files via upload

---
+++ M1_NPM_RAFI_NAUFAL_SYAH.py
@@ -0,0 +1,12 @@
+#Diketahui dimensi balok
+a = 12
+b = 10
+c = 15
+
+#Perhitungan volume balok
+volume_balok = a * b * c
+print(volume_balok)
+
+#Perhitungan luas permukaan balok
+luas_permukaan_balok = 2 * (a * b + b * c + a * c)
+print(luas_permukaan_balok)
